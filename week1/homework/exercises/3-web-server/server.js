019cff9a2a159fac1396ae6efa1282dff269331c
Ahmad Magdy <amagsid@gmail.com>
2021-01-31 08:40:16 +0100

    finished all JS exercises

--- week1/homework/exercises/3-web-server/server.js
+++ week1/homework/exercises/3-web-server/server.js
@@ -2,13 +2,56 @@
  * Exercise 3: Create an HTTP web server
  */
 
-var http = require('http');
+let http = require("http");
+const fs = require("fs");
 
-//create a server
-let server = http.createServer(function (req, res) {
-  // YOUR CODE GOES IN HERE
-	res.write('Hello World!'); // Sends a response back to the client
-	res.end(); // Ends the response
+const server = http.createServer((req, res) => {
+  console.log(req.url);
+  if (req.url === "/") {
+    fs.readFile("index.html", null, function (error, data) {
+      if (error) {
+        res.writeHead(404);
+        res.write("index.html not found!");
+      } else {
+        res.writeHead(200, {
+          "Content-Type": "text/html",
+        });
+        res.write(data);
+        res.end();
+      }
+    });
+  }
+
+  if (req.url === "/index.js") {
+    fs.readFile("index.js", null, function (error, data) {
+      if (error) {
+        res.writeHead(404);
+        res.write("index.js not found!");
+      } else {
+        res.writeHead(200, {
+          "Content-Type": "text/javascript",
+        });
+        res.write(data);
+        res.end();
+      }
+    });
+  }
+
+  if (req.url === "/style.css") {
+    fs.readFile("style.css", null, function (error, data) {
+      if (error) {
+        res.writeHead(404);
+        res.write("style.css not found!");
+      } else {
+        res.writeHead(200, {
+          "Content-Type": "text/css",
+        });
+        res.write(data);
+        res.end();
+      }
+    });
+  }
 });
 
-server.listen(3000); // The server starts to listen on port 3000
+//I set the port to 8000 since 3000 kept giving mt an error that it's in use
+server.listen(8000);
